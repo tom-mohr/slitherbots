9a0786fac91f399ae11d72ca7b0ed6cc4289e1e1
andi <you@example.com>
2022-05-22 22:39:57 +0200

    aa

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#from slitherbots import World
+from slitherbots import World
 from utils import get_snakes
-get_snakes()
-#world = slitherbots.World(get_snakes())
+
+world = World(get_snakes())
 
 
