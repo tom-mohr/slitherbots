0496ab385e21495a1b8f759249ca75a306815faf
tom-mohr <24864629+tom-mohr@users.noreply.github.com>
2022-05-22 13:59:01 +0200

    wip

--- src/include/logic.h
+++ src/include/logic.h
@@ -11,12 +11,22 @@ class Api {
 class Snake {
     public:
         Snake();
+        //todo: username
+        //todo: step method from user
+        double sight_radius;
+        vector<Segment> segments;
+        Segment& get_head();  // utility method to get segements[0]
+        void apply(Api api);  // translate api response to snake movement
+    private:
+};
+
+class Segment {
+    public:
+        Segment();
         double x;
         double y;
         double radius;
         double angle;
-    private:
-
 };
 
 class World {
--- src/src/logic.cpp
+++ src/src/logic.cpp
@@ -3,8 +3,27 @@
 
 using namespace std;
 
-Snake::Snake() {
-    radius = 10;
+Snake::Snake(/*name, step*/) {//todo: pass name and step method from user
+    sight_radius;
+}
+
+Segment& Snake::get_head() {
+    return segments[0];
+}
+
+void Snake::apply(Api api) {
+    Segment& head = get_head();
+    head.angle += clamp(api.angle, -0.5, 0.5);
+}
+
+double clamp(double value, double min, double max) {
+    if (value < min) {
+        return min;
+    } else if (value > max) {
+        return max;
+    } else {
+        return value;
+    }
 }
 
 double mod(double a, double b) {
@@ -13,11 +32,35 @@ double mod(double a, double b) {
     return i;
 }
 
+double distance(double x1, double y1, double x2, double y2) {
+    return hypot(x2 - x1, y2 - y1);
+}
+
+double distance(Segment& a, Segment& b) {
+    return distance(a.x, a.y, b.x, b.y);
+}
+
 void World::step() {
     for (auto& snake : snakes) {
+        Api api;
+
+        Segment& head = snake.get_head();
+
         for (auto& other : snakes) {
+            for (auto& seg : other.segments) {
+                double dist = distance(head, seg);
+                if (dist <= snake.sight_radius) {
+
+                }
+            }
             double angleDiff = other.angle - snake.angle;
-            angleDiff = mod(angleDiff + M_PI, M_2_PI) - M_PI;
+            angleDiff = mod(angleDiff + M_PI, M_2_PI) - M_PI;// angle in interval [-pi, +pi)
+            api.segments.push_back(seg);
         }
+
+        //todo: execute user code: snake.step(api);
+
+        // apply api object
+        snake.apply_api(api);
     }
 }
