6cd83992d30c76c05ea63216d726b62298b337c7
tom-mohr <24864629+tom-mohr@users.noreply.github.com>
2022-05-22 13:23:28 +0200

    wip

--- src/src/logic.cpp
+++ src/src/logic.cpp
@@ -1,11 +1,23 @@
 #include "logic.h"
+#include <math.h>
+
+using namespace std;
 
 Snake::Snake() {
     radius = 10;
 }
 
+double mod(double a, double b) {
+    int i = fmod(a, b);
+    if (i<a) i += b;
+    return i;
+}
+
 void World::step() {
     for (auto& snake : snakes) {
-        
+        for (auto& other : snakes) {
+            double angleDiff = other.angle - snake.angle;
+            angleDiff = fmod
+        }
     }
 }
